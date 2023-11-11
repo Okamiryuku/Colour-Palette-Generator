d7819fc613de272c3ba12a12222c080eccf713c2
Okamiryuku <okamiryuku@gmail.com>
2023-11-11 11:42:10 -0600

    Starting the Web APP

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from PIL import Image
+from flask import Flask, render_template
 
 # Load the image using PIL
 file_name = 'sample_img.jpeg'
@@ -24,8 +25,20 @@ for i in range(height):
         pixel_counts[pixel_value] = pixel_counts.get(pixel_value, 0) + 1
 
 # Get the top 10 repeated values
-top_10_values = sorted(pixel_counts.items(), key=lambda x: x[1], reverse=True)[:10]
+top_10_values = sorted(pixel_counts.items(), key=lambda x: x[1], reverse=True)[:12]
 
 # Display or save the top 10 values
 for value, count in top_10_values:
     print(f'Pixel Value: {value}, Count: {count}')
+
+# Creating my Flask APP
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
